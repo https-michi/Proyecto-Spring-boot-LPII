068e7e42b5c5d5f1b9ef8bb4d786e29e0a51c74d
https-michi <glitchmichi@gmail.com>
2024-06-21 19:50:56 -0500

    init: Se inicializa el proyecto con una estructura básica

---
+++ src/test/java/pro/web/demo/v1/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.web.demo.v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
